15f852a7d2032a3e5f52077599a49023a3779f48
Grootcode21 <antonyndichu15@gmail.com>
2026-02-04 18:16:28 +0300

    app registration

--- localshop/settings.py
+++ localshop/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'localshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
