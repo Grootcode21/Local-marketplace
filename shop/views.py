ce96740fba265473d75c325b339bfc407c1a33b8
Grootcode21 <antonyndichu15@gmail.com>
2026-02-07 05:57:18 +0300

    views & urls

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Product, Category
+from .forms import ProductForm
+
+
+def home(request):
+    products = Product.objects.all().order_by('-created_at')
+    categories = Category.objects.all()
+    return render(request, 'shop/home.html', {'products': products, 'categories': categories})
+
+
+@login_required
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save(commit=False)
+            product.seller = request.user
+            product.save()
+            return redirect('home')
+    else:
+        form = ProductForm()
+    return render(request, 'shop/add_product.html', {'form': form})
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'shop/product_detail.html', {'product': product})
+
+
+def category_filter(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    products = Product.objects.filter(category=category)
+    return render(request, 'shop/home.html', {'products': products})
